f437a74789626c0b771fbc99d9a125d3d1bd9eda
Dee'Kej <deekej@linuxmail.org>
2014-03-25 02:53:36 +0100

    Testing of GIT FLOW.

---
+++ xml2ddl/analyzer.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3.2
+# -*- coding: utf-8 -*-
+#XTD:xkaspa34
+
+# ============================================================================= #
+#
+# File (module): analyzer.py
+# Version:       0.0.0.1
+# Start date:    23-03-2014
+# Last update:   25-03-2014
+#
+# Course:        IPP (summer semester, 2014)
+# Project:       Script for converting XML format into DDL (SQL) format,
+#                written in Python 3.2.3.
+#
+# Author:        David Kaspar (aka Dee'Kej), 3BIT
+# 
+# Faculty:       Faculty of Information Technologies,
+#                Brno University of Technologies,
+#                Czech Republic
+#
+# E-mail:        xkaspa34@stud.fit.vutbr.cz
+#
+# Description:   See the module doc-string.
+#
+# More info @:   https://www.fit.vutbr.cz/study/courses/index.php?id=9384 
+#
+# File encoding: en_US.utf8 (United States)
+#
+# ============================================================================= #
+
+# ==================
+# Module doc-string:
+# ==================
+"""\
+"""
+
+
+# ===============
+# Public Classes:
+# ===============
+        
+
+# ===================
+# Internal functions:
+# ===================
+def _main():
+
+if __name__ == '__main__':
+    import sys
+    status = _main()
+    sys.exit(status)
+
